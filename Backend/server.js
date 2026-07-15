4bad92ae29f924b8dcf218125914355006564bf7
tanishq singhal <ts2311180@gmail.com>
2026-07-15 21:25:28 +0530

    Connect watchlist with backend APIs

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import favouriteRoutes from "./routes/FavouritesRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/favourites", favouriteRoutes);
 
