0f3cd6cbe6f80afeff701ea54ac787915b089715
tanishq singhal <ts2311180@gmail.com>
2026-07-22 13:08:41 +0530

    Fixed missing Featured css import

--- Frontend/src/components/Featured.jsx
+++ Frontend/src/components/Featured.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Featured.css";
+import "./featured.css";
 import { useNavigate } from "react-router-dom";
 
 const Featured = ({ movies, search, loading }) => {
