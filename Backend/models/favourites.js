c3b2249ad143b31c6f826eec5261464300681d27
tanishq singhal <ts2311180@gmail.com>
2026-07-22 14:44:10 +0530

    Fix favourite user mapping

--- Backend/models/favourites.js
+++ Backend/models/favourites.js
@@ -1,20 +1,19 @@
 import mongoose from "mongoose";
 
-
 const favouriteSchema = new mongoose.Schema({
-  userID:String,
-    omdbID: String,
-    title: String,
-    poster: String,
-    year: String,
-    genre: String,
-    rating: String,
-})
-  
+  userId:String,
+  omdbID:String,
+  title:String,
+  poster:String,
+  year:String,
+  genre:String,
+  rating:String,
+},{
+  timestamps:true
+});
 
+favouriteSchema.index({userId:1,omdbID:1},{unique:true});
 
-favouriteSchema.index({ userID: 1, omdbID: 1 }, { unique: true });
-
-const Favourite = mongoose.model("Favourite", favouriteSchema);
+const Favourite=mongoose.model("Favourite",favouriteSchema);
 
 export default Favourite;
