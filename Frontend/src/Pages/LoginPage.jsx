8717e5e5661c9492b45078154725ebfe366fde30
tanishq singhal <ts2311180@gmail.com>
2026-07-22 14:06:20 +0530

    Fixed login authentication flow

--- Frontend/src/Pages/LoginPage.jsx
+++ Frontend/src/Pages/LoginPage.jsx
@@ -17,7 +17,7 @@ const LoginPages = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://cine-vaultt-1.onrender.com/api/auth/login", {
+      const response = await fetch("https://cine-vaultt-2.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
