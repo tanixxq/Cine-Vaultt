114f5b6a9f1aa2a3e9d805d1889ac92812d4c520
tanishq singhal <ts2311180@gmail.com>
2026-07-22 12:49:15 +0530

    Added authentication state to navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,15 +1,35 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <h1>CineVault</h1>
 
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/watchlist" >❤️ Watchlist</Link></li>
-        <li><Link to="/login" className="login-btn-nav">Login</Link></li>
+        <li><Link to="/watchlist" className="watchlist-btn-nav">❤️ Watchlist</Link></li>
+
+        {user ? (
+          <li>
+            <button 
+              onClick={logout}
+              className="login-btn-nav"
+            >
+              Logout
+            </button>
+          </li>
+        ) : (
+          <li>
+            <Link to="/login" className="login-btn-nav">
+              Login
+            </Link>
+          </li>
+        )}
       </ul>
     </div>
   );
